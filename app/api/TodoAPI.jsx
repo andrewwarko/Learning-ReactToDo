0857fdcb0fe2b17cb3496445ce1d5ab126fc7854
Andrew Warko <andrewwarko@users.noreply.github.com>
2016-09-08 18:33:53 -0400

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,36 @@ module.exports = {
         }
 
         return $.isArray(todos) ? todos : [];
+    },
+    filterTodos: function (todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // Filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        // Filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+
+        // Sort todos with non-completed first
+        // Returning -1 returns a then b
+        // Returning - returns no change
+        // Returning 1 returns b then a
+        filteredTodos.sort((a, b) => {
+            if (a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
     }
 };
