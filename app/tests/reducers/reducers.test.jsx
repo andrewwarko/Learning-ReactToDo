7dcb14fc3788f53799a5b3d6b34bbf6f8de50bfc
Andrew Warko <andrewwarko@users.noreply.github.com>
2016-09-10 01:45:30 -0400

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,4 +1,5 @@
 var expect = require('expect');
+// Checks to make sure the functions act as pure functions correctly.
 var df = require('deep-freeze-strict');
 
 var reducers = require('reducers');
@@ -24,6 +25,37 @@ describe('Reducers', () => {
             var res = reducers.showCompletedReducer(df(false), df(action));
 
             expect(res).toEqual(true);
+        });
+    });
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'Walk the dog'
+            };
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        it('should toggle todo', () => {
+            var todos = [{
+                id: '123',
+                text: 'Something',
+                completed: true,
+                createdAt: 123,
+                completedAt: 125
+            }];
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: '123'
+            };
+            var res = reducers.todosReducer(df(todos), df(action));
+
+            expect(res[0].completed).toEqual(false);
+            expect(res[0].completedAt).toEqual(undefined);
         });
     });
 });
